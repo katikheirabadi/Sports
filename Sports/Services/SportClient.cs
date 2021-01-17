92601f2cab248bbeaf1f5b4553844e6f2120b046
ASUS <kati.kheirabadi@gmail.com>
2021-01-17 12:32:20 +0330

    make client

--- Sports/Services/SportClient.cs
+++ Sports/Services/SportClient.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Sports.Services
 {
     public class SportClient
     {
+        private readonly HttpClient client;
+        private const string BaseAdress = "https://www.thesportsdb.com";
+
+        public SportClient(HttpClient client)
+        {
+            this.client = client;
+            this.client.BaseAddress = new Uri(BaseAdress);
+            this.client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+        }
     }
 }
